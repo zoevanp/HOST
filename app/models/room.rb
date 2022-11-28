09e112ddefe473995b8db3482d547dd6f82e4fff
Zoe vanPerlstein <zoe.vanperlstein1@gmail.com>
2022-11-28 18:25:48 +0000

    validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :host, class_name: 'User'
   has_many :bookings
+
+  validates :beds, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,14 @@ class User < ApplicationRecord
   has_many :bookings_as_host, through: :rooms_as_host, source: :bookings
   has_many :bookings_as_refugee, foreign_key: "refugee_id", class_name: "Booking"
   has_many :rooms_as_refugee, through: :bookings_as_refugee, source: :rooms
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :role, presence: true
+  if :role == "host"
+    validates :host_id_number, presence: true
+  elsif :role == "refugee"
+    validates :refugee_id_number, presence: true
+  end
 end
