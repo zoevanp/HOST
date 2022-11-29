97717013d912ca51deb58d7cc8f049ac3c6bde29
Zoe vanPerlstein <zoe.vanperlstein1@gmail.com>
2022-11-29 12:20:15 +0000

    destroy in rooms controller

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
-  belongs_to :room, dependent: :destroy
-  belongs_to :refugee, class_name: 'User', dependent: :destroy
-  has_one :host, through: :room, dependent: :destroy
+  belongs_to :room, dependent: :delete
+  belongs_to :refugee, class_name: 'User', dependent: :delete
+  has_one :host, through: :room, dependent: :delete
 
   validates :arrival_date, presence: true
   validates :departure_date, presence: true
