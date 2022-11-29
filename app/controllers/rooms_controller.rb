f2c4edd5cea7778bddf40b3bb7e096df29b05a97
samuel van de ven <samuel.vandeven@gmail.com>
2022-11-29 11:45:08 +0000

    rooms controller + views

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,51 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: %i[show edit update destroy]
+  def index
+    @rooms = Room.where(host_id: current_user.id)
+  end
+
+  def show
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @room.host_id = current_user.id
+    if @room.save
+      redirect_to rooms_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to room_path(@room)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    redirect_to rooms_path
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:beds)
+  end
+
+end
