03f3b6cb104d0f61e9ab0635d33fbc35e42f914f
Zoe vanPerlstein <zoe.vanperlstein1@gmail.com>
2022-11-29 12:56:05 +0000

    dependencies again

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
-  belongs_to :room, dependent: :delete_all
-  belongs_to :refugee, class_name: 'User', dependent: :destroy
-  has_one :host, through: :room, dependent: :destroy
+  belongs_to :room
+  belongs_to :refugee, class_name: 'User'
+  has_one :host, through: :room
 
   validates :arrival_date, presence: true
   validates :departure_date, presence: true
