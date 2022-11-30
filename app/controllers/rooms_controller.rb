7b8a6119ab1be5eb63edf011bb92d51e3218fdd6
samuel van de ven <samuel.vandeven@gmail.com>
2022-11-30 15:04:06 +0000

    added cloudinary to profile and rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -26,10 +26,10 @@ class RoomsController < ApplicationController
   end
 
   def update
-    raise
     if @room.update(room_params)
-      redirect_to room_path(@room)
+      redirect_to room_path(@room), notice: "Room was successfully updated"
     else
+      raise
       render :edit, status: :unprocessable_entity
     end
   end
@@ -46,6 +46,6 @@ class RoomsController < ApplicationController
   end
 
   def room_params
-    params.require(:room).permit(:beds, :profile_picture)
+    params.require(:room).permit(:beds, room_pictures: [])
   end
 end
