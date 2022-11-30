dde6d77f26f2563c6c967c65a7e699b061c0d18c
samuel van de ven <samuel.vandeven@gmail.com>
2022-11-30 17:18:07 +0000

    revies + validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :room
   belongs_to :refugee, class_name: 'User'
   has_one :host, through: :room
+  has_many :reviews
 
   validates :arrival_date, presence: true
   validates :departure_date, presence: true
