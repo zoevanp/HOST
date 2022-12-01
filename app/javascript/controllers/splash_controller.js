33675277f7f8a75ee590136ab8af60d329258f0d
samuel van de ven <samuel.vandeven@gmail.com>
2022-12-01 13:37:53 +0000

    homepage register

--- app/javascript/controllers/splash_controller.js
+++ app/javascript/controllers/splash_controller.js
@@ -4,18 +4,25 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ["enterHostContainer", "whiteContainer", "button", "loginForm"]
+  static targets = ["enterHostContainer", "whiteContainer", "button", "loginForm", "registerForm"]
 
   slide() {
     this.enterHostContainerTarget.classList.add("left-container")
     this.whiteContainerTarget.classList.add("right-container")
     this.whiteContainerTarget.classList.remove("d-none")
-    this.buttonTarget.classList.add("d-none")
+    this.buttonTarget.classList.add("color: orange")
   }
 
-  showForm(event) {
+  showLoginForm(event) {
     event.preventDefault()
     this.whiteContainerTarget.classList.add("d-none")
     this.loginFormTarget.classList.remove("d-none")
+  }
+
+  showSignUpForm(event) {
+    event.preventDefault()
+    this.whiteContainerTarget.classList.add("d-none")
+    this.registerFormTarget.classList.remove("d-none")
+
   }
 }
