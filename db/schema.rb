ccf7ec9539c9766ba304bc034b471222c9446ed7
samuel van de ven <samuel.vandeven@gmail.com>
2022-12-05 10:17:40 +0000

    remove-migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_163058) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_180012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_163058) do
   end
 
   create_table "bookings", force: :cascade do |t|
+    t.date "arrival_date"
+    t.date "departure_date"
     t.bigint "room_id", null: false
     t.bigint "refugee_id", null: false
     t.datetime "created_at", null: false
@@ -52,12 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_163058) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
-
     t.integer "beds"
-
-    t.date "arrival_date"
-    t.date "departure_date"
-
     t.index ["refugee_id"], name: "index_bookings_on_refugee_id"
     t.index ["room_id"], name: "index_bookings_on_room_id"
   end
