6c7833c0f0617c1eb962aa54b9b6ba93c8a5add6
Zoe vanPerlstein <zoe.vanperlstein1@gmail.com>
2022-12-05 10:41:20 +0000

    text

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_163058) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_180012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,12 +50,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_04_163058) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
-
-    t.integer "beds"
-
     t.date "arrival_date"
     t.date "departure_date"
-
+    t.integer "beds"
     t.index ["refugee_id"], name: "index_bookings_on_refugee_id"
     t.index ["room_id"], name: "index_bookings_on_room_id"
   end
