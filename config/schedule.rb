b32c8c6886e481fe454c33b6427d360256e506c7
samuel van de ven <samuel.vandeven@gmail.com>
2022-12-05 18:10:07 +0000

    seebookingbutton

---
+++ config/schedule.rb
@@ -0,0 +1,40 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+
+
+# every 1.day, at: '6:00 pm' do
+#   # command "/usr/bin/some_great_command"
+#   runner "update_bookings"
+#   # rake "some:great:rake:task"
+# end
+
+# every 5.seconds do
+#   # command "/usr/bin/some_great_command"
+#   runner "Booking.update_bookings"
+#   # rake "some:great:rake:task"
+# end
+
+
+
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+#  # every 1.day, at: '6:00 pm' do
+#   #   runner update_bookings
+#   # end
+
+#   every 2.hours do # 1.minute 1.day 1.week 1.month 1.year is also supported
+#     # the following tasks are run in parallel (not in sequence)
+#     runner update_bookings
+#   end
