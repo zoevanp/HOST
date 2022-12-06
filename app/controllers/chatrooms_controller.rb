a9f4e445d7514e995ed08f9dd157d9bb251e4ea0
samuel van de ven <samuel.vandeven@gmail.com>
2022-12-06 16:00:44 +0000

    booking-chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,22 +1,17 @@
 class ChatroomsController < ApplicationController
 
-  def new
-    @chatroom = Chatroom.new
-    @booking = Booking.find(params[:booking_id])
-  end
+  # def create
+  #   @chatroom = Chatroom.new
+  #   @booking = Booking.find(params[:booking_id])
+  #   @chatroom.booking_id = Booking.find(params[:booking_id]).id
+  #   @chatroom.name = "Chatroom"
 
-  def create
-    @chatroom = Chatroom.new
-    @booking = Booking.find(params[:booking_id])
-    @chatroom.booking_id = Booking.find(params[:booking_id]).id
-    @chatroom.name = "Chatroom"
-
-    if @chatroom.save
-      redirect_to booking_chatroom_path(@booking, @chatroom)
-    else
-      raise
-    end
-  end
+  #   if @chatroom.save
+  #     redirect_to booking_chatroom_path(@booking, @chatroom)
+  #   else
+  #     raise
+  #   end
+  # end
 
   def show
     @chatroom = Chatroom.find(params[:id])
