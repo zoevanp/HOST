a5d0f14e89bacbccad7b1056601b936b2d49166a
Zoe vanPerlstein <zoe.vanperlstein1@gmail.com>
2022-12-07 10:37:46 +0000

    metatags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -9,6 +8,10 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+  end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
   add_flash_types :info, :error, :success
